a2cdd9b8e1b501bf3548b0130e4f1be7de16e3f0
Rakotondramanana Manda Fanaiky <manda-fanaiky.rakotondramanana.1@ulaval.ca>
2026-06-21 01:21:46 -0400

    routes pour views + squelette

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,39 @@
 import { useState } from 'react'
 import './App.css'
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Transactions from './Pages/Transactions'
+import Comptes from './Pages/Comptes'
+import Logo from './assets/profile.svg?react'
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     
     <div className="flex h-screen "> {/* wrapper flex */}
-      <aside className="bg-emerald-700 h-full min-w-1/4 rounded-br-lg">
+      <aside className="bg-emerald-700 h-full min-w-1/4 
+      rounded-br-lg flex flex-col"> {/* sidebar */}
       <Navbar />
+
+      <main className="flex-1" />
+
+      <footer className=" min-h-24 text-neutral-100 
+                antialiased flex justify-center gap-5"> 
+        <div>
+              <Logo  className='max-h-24 w-15 fill-white'/>
+        </div>
+        <p className='text-lg font-semibold place-self-center'>Profil</p>
+      </footer>
     </aside>
+
+    <div className="p-4">
+        <Routes>
+          <Route path='/transactions' element={<Transactions/>} />
+          <Route path='/comptes' element={<Comptes/>} />
+        </Routes>
+      </div>
 
     </div>
 
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,23 +1,54 @@
 //@ts-check
+import { NavLink } from "react-router-dom";
 
 function Navbar() {
+  return (
+    <>
+      <div className="flex items-center py-8">
+        <img
+          src="/src/assets/personal-finance.png"
+          alt="Logo"
+          className="w-16 h-16 ml-4 mr-4"
+        />
+        <h1 className="text-neutral-100 text-3xl font-mono">F-track</h1>
+      </div>
 
-
-    return (
-        <>
-        <h1 className="text-neutral-100 mb-8 text-3xl indent-8">F-track</h1>
-        <ol className="grid grid-cols-2 md:grid-cols-1 gap-y-6.25">
-            <li className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
-            antialiased rounded-s-lg text-center hover:bg-emerald-950" >Dashboard</li>
-            <li className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
-            antialiased rounded-s-lg text-center hover:bg-emerald-950">Comptes</li>
-            <li className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
-            antialiased rounded-s-lg text-center hover:bg-emerald-950">Transactions</li>
-            <li className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
-            antialiased rounded-s-lg text-center hover:bg-emerald-950">Budgets</li>
-        </ol>
-        </>
-    )
+      <ol className="grid grid-cols-2 md:grid-cols-1 gap-y-2">
+        <NavLink to="/dashboard">
+          <li
+            className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
+                antialiased rounded-s-lg text-center hover:bg-emerald-800"
+          >
+            Dashboard
+          </li>
+        </NavLink>
+        <NavLink to="/comptes">
+          <li
+            className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
+                antialiased rounded-s-lg text-center hover:bg-emerald-800"
+          >
+            Comptes
+          </li>
+        </NavLink>
+        <NavLink to="/transactions">
+          <li
+            className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
+                antialiased rounded-s-lg text-center hover:bg-emerald-800"
+          >
+            Transactions
+          </li>
+        </NavLink>
+        <NavLink to="/budgets">
+          <li
+            className=" hover:600 dark:hover:shadow-xl/30 text-neutral-100 
+                antialiased rounded-s-lg text-center hover:bg-emerald-800"
+          >
+            Budgets
+          </li>
+        </NavLink>
+      </ol>
+    </>
+  );
 }
 
 export default Navbar;
